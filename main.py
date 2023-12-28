08d9a1c20605eb7574a82f2a52fc06157c5cdc80
AlexanderVVasilenko <alexander_vasilenko@outlook.com>
2023-12-28 13:06:59 +0200

    main.py: deleted comments

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ def reverse_words(text: str) -> str:
     reversed_words = []
 
     for word in text.split():
-        # Create a list of [position, character] for each symbol
         symbols = [[len(word) - index - 1, c] if c.isalpha() else [index, c] for index, c in enumerate(word)]
 
-        # Sort the list based on the position and reconstruct the word
         reversed_word = ''.join(symbol[1] for symbol in sorted(symbols, key=lambda x: x[0]))
         reversed_words.append(reversed_word)
 
-    # Join the reversed words to form the final result
     result = ' '.join(reversed_words)
     return result
 
